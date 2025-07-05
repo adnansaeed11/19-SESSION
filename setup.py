4881b85f067374f71056038795ecd59de6ed24f0
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2025-07-05 18:49:57 +0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='session 19 of mlops just for practicing',
+    author='Adnan',
+    license='MIT',
+)
